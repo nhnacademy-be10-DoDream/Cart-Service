761cc28d8c07e6193b3b840cb5d19a2d8e16c901
gunheecha <gunhee2326@naver.com>
2025-07-11 17:56:20 +0900

    refactor: delete single
    - cascade 추가
    
    #77

--- src/main/java/shop/dodream/cart/entity/CartItem.java
+++ src/main/java/shop/dodream/cart/entity/CartItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,6 +27,7 @@ public class CartItem {
 	//카트고유ID
 	@ManyToOne
 	@JoinColumn(name = "cart_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Cart cart;
 	//정가
 	private Long salePrice;
