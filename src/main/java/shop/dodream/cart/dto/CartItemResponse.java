dccb59ad383e598fd6d1b141561e6f1bb3428138
gunheecha <gunhee2326@naver.com>
2025-07-16 11:07:40 +0900

    refactor: update cart
    - validation 삭제
    
    #92

--- src/main/java/shop/dodream/cart/dto/CartItemResponse.java
+++ src/main/java/shop/dodream/cart/dto/CartItemResponse.java
@@ -11,18 +11,11 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 public class CartItemResponse implements Serializable {
-	@NotNull
 	private Long cartItemId;
-	@NotNull
 	private Long bookId;
-	@NotNull
 	private String title;
-	@NotNull
 	private Long salePrice;
-	@NotNull
-	@Min(1)
 	private Long quantity;
-	@NotNull
 	private String bookUrl;
 	
 	public static CartItemResponse of(CartItem item, BookListResponseRecord book) {
--- src/main/java/shop/dodream/cart/dto/GuestCartItem.java
+++ src/main/java/shop/dodream/cart/dto/GuestCartItem.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GuestCartItem {
-	@NotNull
 	private Long bookId;
 	@NotNull
 	@Min(1)
